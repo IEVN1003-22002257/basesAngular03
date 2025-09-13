ef0fc9b564c1e9e176d49fcadd816de351da343b
IEVN1003-22002257 <jonymendez40@gmail.com>
2025-09-12 18:48:21 -0600

    Primer comit

---
+++ src/app/heroes/heroes-list/heroes-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  standalone: false,
+  templateUrl: './heroes-list.component.html',
+  styleUrl: './heroes-list.component.css'
+})
+export class HeroesListComponent {
+
+}
