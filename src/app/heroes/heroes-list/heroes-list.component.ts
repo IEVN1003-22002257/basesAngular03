b0e4fdbc1564e889a6b118f377e3051c459b9ac5
IEVN1003-22002257 <jonymendez40@gmail.com>
2025-09-17 19:41:26 -0600

    Filtros

--- src/app/heroes/heroes-list/heroes-list.component.ts
+++ src/app/heroes/heroes-list/heroes-list.component.ts
@@ -3,9 +3,49 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-list',
   standalone: false,
-  templateUrl: './heroes-list.component.html',
-  styleUrl: './heroes-list.component.css'
+  templateUrl: './heroes-list.component.html'
 })
 export class HeroesListComponent {
 
+  imageWidth:number=40;
+  imageMargin:number=2;
+  muestraImage:boolean=true;
+  listFilter:string=''
+
+  showImage():void{
+    this.muestraImage=!this.muestraImage;
+  }
+
+  heroes:any[]=[
+    {
+    imagen:"https://dragonball-api.com/characters/goku_normal.webp",
+    nombre:"Goku",
+    description:"KameHameHa",
+    race:"Saiyan",
+    ki:90000
+    },
+    {
+    imagen:"https://dragonball-api.com/characters/vegeta_normal.webp",
+    nombre:"Vegeta",
+    description:"Final Flash",
+    race:"Saiyan",
+    ki:19000
+    },
+    {
+    imagen:"https://dragonball-api.com/characters/picolo_normal.webp",
+    nombre:"Piccolo",
+    description:"Malanasapo",
+    race:"Namekiano",
+    ki:70000
+    },
+     {
+    imagen:"https://dragonball-api.com/characters/Freezer.webp",
+    nombre:"Freezer",
+    description:"Death Ball",
+    race:"Alien",
+    ki:70000
+    },
+ 
+  ]
+  
 }
